524a804ae1af25bda84ef9199b3350c4a22fea25
Sergii Kamenskyi <serg@apple.com>
2022-10-14 20:57:04 +0300

    Initial commit...

--- models/tags.py
+++ models/tags.py
@@ -4,5 +4,5 @@ class Wim_Tags(db.Model):
     __tablename__ = 'tags'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    wim_id = db.Column(db.Integer, db.ForeignKey("wims.id"))
+    # user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    # wim_id = db.Column(db.Integer, db.ForeignKey("wims.id"))
